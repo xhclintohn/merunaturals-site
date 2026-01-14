3062bad4988cc880dc62df49749b0fad5bfbbfb6
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-01-14 14:38:48 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

--- config.js
+++
@@ -1,8 +0,0 @@
-const MERU_CONFIG = {
-    GITHUB_TOKEN: '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',
-    REPO_OWNER: 'xhclintohn',
-    REPO_NAME: 'merunaturals-site',
-    BRANCH: 'main',
-    ADMIN_PASSWORD: 'Mugambi@2020',
-    API_BASE: 'https://api.github.com'
-};
